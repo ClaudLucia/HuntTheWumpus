d218f68ad889fb52877485cef70494d729ed6e4f
nisiya <syni679@gmail.com>
2018-05-04 16:14:27 -0400

    checked user command before posting

--- HuntTheWumpus/src/Client.hs
+++ HuntTheWumpus/src/Client.hs
@@ -66,17 +66,46 @@ clientStart uri msg = do
     usrCmd <- putStr msg >> hFlush stdout >> getLine
     if (usrCmd == "i")
         then clientStart uri instructions
-        else clientLoop uri response
+        else (if (usrCmd == "y")
+                then clientLoop uri response
+                else clientStart uri "Invalid command.\n\
+                                     \Enter [y] to begin. \n\
+                                     \Enter [i] to view instructions. \n")
   where
     response = ServerMsg 4 gameStart
-    
+
+checkStartCmd :: URI -> String -> IO ()
+checkStartCmd uri "i" = do
+    clientStart uri instructions
+checkStartCmd uri "y" = do
+    clientLoop uri response
+        where
+            response = ServerMsg 4 gameStart
+checkStartCmd uri _ = do
+    clientStart uri "Invalid command.\n\
+                    \Enter [y] to begin. \n\
+                    \Enter [i] to view instructions. \n"
+
 clientLoop :: URI -> ServerMsg -> IO ()
 clientLoop uri response = do
     command <- putStr (msg response) >> hFlush stdout >> getLine
     let usrCmd = words command
-    let input = UserInput (newRoom response) (head usrCmd) (read (last usrCmd))
+    checkGameCmd uri (newRoom response) usrCmd
+
+checkGameCmd :: URI -> Int -> [String] -> IO ()
+checkGameCmd uri currRoom ["move", r] = do
+    let input = UserInput currRoom "move" (read r)
     newRsp <- submitCmd input uri
     checkResult uri newRsp
+checkGameCmd uri currRoom ["shoot", r] = do
+    let input = UserInput currRoom "shoot" (read r)
+    newRsp <- submitCmd input uri
+    checkResult uri newRsp
+checkGameCmd uri currRoom usrCmd = do
+    let response = ServerMsg currRoom "Invalid command. \n\
+                                        \Enter [move #] or [shoot #] where #\
+                                        \is the target room.\n"
+    clientLoop uri response
 
 checkResult :: URI -> String -> IO()
 checkResult uri rsp = do
